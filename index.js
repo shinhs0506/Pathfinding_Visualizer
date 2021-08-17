b5889dd1bf38873a67506d661b4f8b22ac353fe0
John Shin <shinhs0506@gmail.com>
2021-08-16 22:31:48 -0700

    added functionality to collect board configuration when start button is clicked

--- index.js
+++ index.js
@@ -130,3 +130,35 @@ function toggleCreateWall() {
         }
     }
 }
+
+function startTraversal() {
+    let boardConfig = {};
+    let start = document.getElementsByClassName("start");
+    let finish = document.getElementsByClassName("finish");
+    
+    let startCoordinate = parseCoordinate(start[0].id);
+    let finishCoordinate = parseCoordinate(finish[0].id);
+
+    let walls = document.getElementsByClassName("wall");
+
+    let wallCoordinates = [];
+    for (let i = 0; i < walls.length; i++){
+        wallCoordinate = parseCoordinate(walls[i].id);
+        wallCoordinates.push(wallCoordinate);
+
+    }
+
+    boardConfig["start"] = startCoordinate;
+    boardConfig["finish"] = finishCoordinate;
+    boardConfig["walls"] = wallCoordinates;
+
+    console.log(boardConfig);
+}
+
+function parseCoordinate(id) {
+    const parts = id.split('-'); 
+    let coordinate = [];
+    coordinate[0] = parseInt(parts[1], 10);
+    coordinate[1] = parseInt(parts[2], 10);
+    return coordinate;
+}
