df7a41e7d4d74cc40878db86d3aa4f2ba57da35b
John Shin <shinhs0506@gmail.com>
2021-08-17 22:44:56 -0700

    refactoring to pathfinder class

--- index.js
+++ index.js
@@ -131,34 +131,12 @@ function toggleCreateWall() {
     }
 }
 
+import Pathfinder from './Pathfinder.js'
+
+let pathFinder = new Pathfinder();
+
+
 function startTraversal() {
-    let boardConfig = {};
-    let start = document.getElementsByClassName("start");
-    let finish = document.getElementsByClassName("finish");
-    
-    let startCoordinate = parseCoordinate(start[0].id);
-    let finishCoordinate = parseCoordinate(finish[0].id);
-
-    let walls = document.getElementsByClassName("wall");
-
-    let wallCoordinates = [];
-    for (let i = 0; i < walls.length; i++){
-        wallCoordinate = parseCoordinate(walls[i].id);
-        wallCoordinates.push(wallCoordinate);
-
-    }
-
-    boardConfig["start"] = startCoordinate;
-    boardConfig["finish"] = finishCoordinate;
-    boardConfig["walls"] = wallCoordinates;
-
-    console.log(boardConfig);
+    pathFinder.findPath();
 }
 
-function parseCoordinate(id) {
-    const parts = id.split('-'); 
-    let coordinate = [];
-    coordinate[0] = parseInt(parts[1], 10);
-    coordinate[1] = parseInt(parts[2], 10);
-    return coordinate;
-}
