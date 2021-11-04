5e4810a7fb663fffe2a7ee0b01f4d285e1ba647e
John Shin <shinhs0506@gmail.com>
2021-11-03 19:57:23 -0700

    initialize buttons

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <QApplication>
 #include <QWidget>
+#include <QPushButton>
+#include <QGridLayout>
 
 int main(int argc, char *argv[]){
 
     QApplication app(argc, argv);
 
     QWidget window;
+    QPushButton *startButton = new QPushButton("start");
+    QPushButton *clearButton = new QPushButton("clear");
+    QPushButton *resetButton = new QPushButton("reset");
+
+    QGridLayout *layout = new QGridLayout(&window);
+
+    layout->addWidget(startButton, 0, 0);
+    layout->addWidget(clearButton, 0, 1);
+    layout->addWidget(resetButton, 0, 2);
+    
 
     window.resize(250, 150);
     window.setWindowTitle("Pathfinding Visualizer");
