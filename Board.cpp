aa9c4a8a6ba14d583438a0df33ca35b637a55469
John Shin <shinhs0506@gmail.com>
2021-11-19 19:19:15 -0800

    seperated header file for board class

--- Board.cpp
+++ Board.cpp
@@ -1,46 +1,4 @@
-#include<vector>
-
-#include<QWidget>
-#include<QRect>
-#include<QPainter>
-#include<QColor>
-#include<QMouseEvent>
-#include<QPoint>
-
-#include<iostream>
-using namespace std;
-
-const int CELLSIZE = 20;
-const int EMPTY = 0;
-const int START = 1;
-const int FINISH = 2;
-const int WALL = 3;
-
-class Board : public QWidget {
-    
-    private:
-        bool isMousePressed;
-        int rows;
-        int cols;
-        vector<vector<int>> grid;
-        bool isStartGrabbed;
-        bool isFinishGrabbed;
-        pair<int, int> grabStart;
-
-        int getBoardWidth();
-        int getBoardHeight();
-
-    protected:
-        void paintEvent(QPaintEvent *ev) override;
-        void mousePressEvent(QMouseEvent *ev) override;
-        void mouseMoveEvent(QMouseEvent *ev) override;
-        void mouseReleaseEvent(QMouseEvent *ev) override;
-
-    public:
-        Board();
-        void initialize();
-		void hello();
-};
+#include<./Board.h>
 
 Board::Board() {
     isMousePressed = false;
