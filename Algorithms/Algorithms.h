f528356ae35c48ec637b8d9cb9fd9ddbdd178463
John Shin <shinhs0506@gmail.com>
2021-11-20 13:18:24 -0800

    structured algorithm classes

---
+++ Algorithms/Algorithms.h
@@ -0,0 +1,7 @@
+#include <vector>
+#include <utility>
+
+class Algorithms {
+    public :
+        virtual std::vector<std::pair<int, int>> solve(std::vector<int, int> *board) = 0;
+};
---
+++ Algorithms/BFS.cpp
@@ -0,0 +1,11 @@
+#include "Algorithms.h"
+
+class BFS : public Algorithms {
+    public:
+        std::vector<std::pair<int, int>> solve(std::vector<int, int> *board);
+};
+
+std::vector<std::pair<int, int>> BFS::solve(std::vector<int, int> *board) {
+    std::vector<std::pair<int, int>> res;
+    return res;
+}
