8745c696cee271996d9d3d2859127c40ec277cd8
John Shin <shinhs0506@gmail.com>
2021-12-22 00:53:07 -0800

    introduced nested Node class for the AStar algorithm:

--- Algorithms/aStar.cpp
+++ Algorithms/aStar.cpp
@@ -1,25 +1,39 @@
 #include "aStar.h"
 
+AStar::Node::Node(int x, int y, int g, int h, int f) {
+    this->x = x;
+    this->y = y;
+    this->g = g;
+    this->h = h;
+    this->f = f;
+}
+
 Path AStar::solve(Grid grid) {
     std::pair<int, int> start = grid.getStart();
     std::pair<int, int> finish = grid.getFinish();
     std::vector<std::vector<int>> board = grid.getGrid();
 
     // open list
-    set<Node> openSet;
+    std::set<Node> openSet;
     // closed list
-    unordered_set<Node> closedSet;
+    using h = std::hash<int>;
+    int size = board.size();
+    // TODO: come up with a better hash function
+    auto hash = [](const Node& n) {return h()(n.x) + (h()(n.y) * 100);};
+    auto equal = [](const Node& a, const Node& b) {return a.x == b.x && a.y == b.y;};
+    std::unordered_set<Node, decltype(hash), decltype(equal)> closedSet(10, hash, equal);
 
     while(!openSet.empty()) {
-        Node front = openSet.front();
-        openSet.pop_front();
+        std::set<Node>::iterator frontIt = openSet.begin();
+        Node front = *frontIt;
+        openSet.erase(frontIt);
 
         for (std::pair<int, int> dir : Utility::dirs) {
             Node neighbour;
             neighbour.x = front.x + dir.first;
             neighbour.y = front.y + dir.second;
 
-            if (grid.isFinish(neighbour.first, neighbour.second)) {
+            if (grid.isFinish(neighbour.x, neighbour.y)) {
                 // return result;
             }
 
@@ -27,12 +41,12 @@ Path AStar::solve(Grid grid) {
             neighbour.h = euclideanDistance(neighbour.x, neighbour.y, finish.first ,finish.second);
             neighbour.f = neighbour.g + neighbour.h;
 
-            set<Node>::iterator openIt = openSet.find(neighbour);
+            std::set<Node>::iterator openIt = openSet.find(neighbour);
             if (openIt != openSet.end() && openIt->f < neighbour.f) {
                 continue;    
             }
 
-            set<Node>::iterator closedIt = closedSet.find(neighbour);
+            std::unordered_set<Node>::iterator closedIt = closedSet.find(neighbour);
             if (closedIt != closedSet.end() && closedIt->f < neighbour.f) {
                 continue;
             } else {
--- Algorithms/aStar.h
+++ Algorithms/aStar.h
@@ -1,9 +1,23 @@
 #ifndef ASTAR_H
 #define ASTAR_H
 
+#include <set>
+#include <unordered_set>
+
 #include "Algorithms.h"
 
 class AStar : public Algorithms {
+    private:
+        class Node {
+            public:
+                int x;
+                int y;
+                int g;
+                int h;
+                int f;
+                Node();
+                Node(int x, int y, int g, int h, int f);
+        };
     public: 
         Path solve(Grid grid);
 };
