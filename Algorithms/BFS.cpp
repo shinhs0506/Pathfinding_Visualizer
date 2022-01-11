b653b7206166d44b6cd47d46c667ab17e929e32a
John Shin <shinhs0506@gmail.com>
2022-01-11 07:05:00 -0800

    fixed err where visualization for explored cells runs twice

--- Algorithms/BFS.cpp
+++ Algorithms/BFS.cpp
@@ -6,13 +6,14 @@ Path BFS::solve(Grid *grid)
     std::pair<int, int> finish = grid->getFinish();
     std::vector<std::vector<int>> board = grid->getGrid();
     std::vector<std::pair<int, int>> explored;
+    std::vector<std::vector<bool>> visited (board.size(), std::vector<bool>(board[0].size(), false));
     std::queue<std::vector<std::pair<int, int>>> q;
 
     //std::cout << start.first << " " << start.second << std::endl;
     //std::cout << finish.first << " " << finish.second << std::endl;
 
     q.push({start});
-    grid->setExplored(start.first, start.second);
+    visited[start.first][start.second] = true;
 
     while (!q.empty()) {
         std::vector<std::pair<int, int>> front = q.front();
@@ -30,13 +31,13 @@ Path BFS::solve(Grid *grid)
             neighbour.first = last.first + dir.first;
             neighbour.second = last.second + dir.second;
 
-            if (Utility::isInbound(board, neighbour) && grid->isEmpty(neighbour.first, neighbour.second)) {
+            if (Utility::isInbound(board, neighbour) && !visited[neighbour.first][neighbour.second] && !grid->isWall(neighbour.first, neighbour.second)) {
                 std::vector<std::pair<int, int>> newFront(front);
                 newFront.push_back(neighbour);
                 q.push(newFront);
 
                 explored.push_back(neighbour);
-                grid->setExplored(neighbour.first, neighbour.second);
+                visited[neighbour.first][neighbour.second] = true;
 
                 std::cout << "expor " << last.first << " " << last.second << std::endl;
             }
--- Algorithms/DFS.cpp
+++ Algorithms/DFS.cpp
@@ -6,13 +6,13 @@ Path DFS::solve(Grid *grid)
     std::pair<int, int> finish = grid->getFinish();
     std::vector<std::vector<int>> board = grid->getGrid();
     std::vector<std::pair<int, int>> explored;
+    std::vector<std::vector<bool>> visited (board.size(), std::vector<bool>(board[0].size(), false));
     std::stack<std::vector<std::pair<int, int>>> s;
 
     //std::cout << start.first << " " << start.second << std::endl;
     //std::cout << finish.first << " " << finish.second << std::endl;
 
     s.push({start});
-    grid->setExplored(start.first, start.second);
 
     while (!s.empty()) {
         std::vector<std::pair<int, int>> front = s.top();
@@ -25,18 +25,17 @@ Path DFS::solve(Grid *grid)
             return successPath;
         }
 
+        visited[last.first][last.second] = true;
+
         for (std::pair<int, int> dir : Utility::dirs) {
             std::pair<int, int> neighbour;
             neighbour.first = last.first + dir.first;
             neighbour.second = last.second + dir.second;
 
-            if (Utility::isInbound(board, neighbour) && grid->isEmpty(neighbour.first, neighbour.second)) {
+            if (Utility::isInbound(board, neighbour) && !visited[neighbour.first][neighbour.second] && !grid->isWall(neighbour.first, neighbour.second)) {
                 std::vector<std::pair<int, int>> newFront(front);
                 newFront.push_back(neighbour);
                 s.push(newFront);
-
-                explored.push_back(last);
-                grid->setExplored(last.first, last.second);
 
                 std::cout << "expor " << last.first << " " << last.second << std::endl;
 
